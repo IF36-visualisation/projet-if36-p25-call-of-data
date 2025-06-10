338a4c3d6ac73709c0da02dab279a1252294e985
mohammadahamad <mo18ahamad@gmail.com>
2025-06-10 02:21:01 +0200

    Dashboard MAJ nationnalité joueur

--- shiny/server.R
+++ shiny/server.R
@@ -8,6 +8,7 @@ library(plotly)
 
 # 💾 Importation des données (équivalent au chunk Rmd)
 player_stats_2021_2022 <- read.csv("../data/2021-2022 Football Player Stats.csv", sep = ";", fileEncoding = "ISO-8859-1")
+print(colnames(player_stats_2021_2022))
 team_stats_2021_2022   <- read.csv("../data/2021-2022 Football Team Stats.csv", sep = ";", fileEncoding = "ISO-8859-1")
 team_stats_2022_2023   <- read.csv("../data/2022-2023 Football Team Stats.csv", sep = ";", fileEncoding = "ISO-8859-1")
 transfers_2022         <- read.csv("../data/2022_2023_football_summer_transfers.csv", sep = ",", fileEncoding = "UTF-8")
@@ -20,11 +21,17 @@ transfers_clean <- transfers_2022 %>%
 
 transfers_2022$league_from <- sub("^\uFEFF", "", transfers_2022$league_from)
 
+print(colnames(transfers_clean))
+
 
 server <- function(input, output, session) {
   
   # Mise à jour dynamique des filtres dans la sidebar
   updateSelectInput(session, "ligueFilter", choices = sort(unique(transfers_clean$league_from)))
+  updateSelectInput(session, "countryFilter", choices = sort(unique(transfers_clean$country_from)))
+  updateSelectInput(session, "nationalityFilter",
+                    choices = sort(unique(player_stats_2021_2022$Nation)),
+                    selected = NULL)
 
   # Top ligues par nombre de transferts (pour limiter le premier graphique)
   top_ligues <- transfers_clean %>%
@@ -37,13 +44,23 @@ server <- function(input, output, session) {
   
   updateSelectInput(session, "ligueFilter", choices = sort(top_ligues))
   
+  transfers_clean <- transfers_clean %>%
+    left_join(player_stats_2021_2022[, c("Player", "Nation")],
+              by = c("name" = "Player"))
+  
   # Filtrage pour le graphique de volume (top ligues seulement)
   filtered_volume <- reactive({
     df <- transfers_clean %>%
       filter(league_from %in% top_ligues)
+    
     if (!is.null(input$ligueFilter) && length(input$ligueFilter) > 0) {
       df <- df %>% filter(league_from %in% input$ligueFilter)
     }
+    
+    if (!is.null(input$nationalityFilter) && length(input$nationalityFilter) > 0) {
+      df <- df %>% filter(Nation %in% input$nationalityFilter)
+    }
+    
     df
   })
   
@@ -71,14 +88,31 @@ server <- function(input, output, session) {
     datatable(df, options = list(pageLength = 10,lengthChange = FALSE))
   })
   
+  # Récupérer uniquement les pays liés aux top ligues
+  countries_top_ligues <- transfers_clean %>%
+    filter(league_from %in% top_ligues) %>%
+    filter(!str_detect(tolower(league_from), "2|ii|b|3|iii|c")) %>%  # garder que les premières divisions
+    filter(!is.na(country_from) & country_from != "") %>%
+    pull(country_from) %>%
+    unique() %>%
+    sort()
+  
+  # Met à jour dynamiquement le selectInput avec ces pays uniquement
+  updateSelectInput(session, "countryFilter", choices = countries_top_ligues)
+  
   filtered_valeur <- reactive({
     df <- transfers_clean %>%
-      filter(league_from %in% top_ligues) %>%  # garder que les ligues du top volume
-      filter(!str_detect(tolower(league_from), "2|ii|b|3|iii|c"))  # enlever 2e et 3e divisions (plusieurs cas)
+      filter(league_from %in% top_ligues) %>%
+      filter(!str_detect(tolower(league_from), "2|ii|b|3|iii|c"))
     
     if (!is.null(input$countryFilter) && length(input$countryFilter) > 0) {
       df <- df %>% filter(country_from %in% input$countryFilter)
     }
+    
+    if (!is.null(input$nationalityFilter) && length(input$nationalityFilter) > 0) {
+      df <- df %>% filter(Nation %in% input$nationalityFilter)
+    }
+    
     df
   })
   
--- shiny/ui.R
+++ shiny/ui.R
@@ -5,25 +5,41 @@ library(plotly)
 
 ui <- dashboardPage(
   dashboardHeader(title = "Analyse des transferts été 2022", titleWidth = 300),
+  
   dashboardSidebar(
     sidebarMenu(
       menuItem("Volume transferts", tabName = "volume", icon = icon("chart-bar")),
       menuItem("Valeur marchande", tabName = "valeur", icon = icon("money-bill-wave")),
       br(),
-      selectInput("ligueFilter", "Filtrer par ligue de départ :", 
-                  choices = NULL,  # initialisé dynamiquement dans server
+      selectInput("nationalityFilter", "Filtrer par nationalité du joueur :",
+                  choices = NULL,
                   selected = NULL,
                   multiple = TRUE)
     )
   ),
+  
   dashboardBody(
     tabItems(
+      
+      # Onglet Volume des transferts
       tabItem(tabName = "volume",
               h2("Volume des transferts par ligue de départ"),
-              plotlyOutput("volumePlot"),
-              br(),
-              DTOutput("volumeTable")
+              sidebarLayout(
+                sidebarPanel(
+                  selectInput("ligueFilter", "Filtrer par ligue de départ :",
+                              choices = NULL,
+                              selected = NULL,
+                              multiple = TRUE)
+                ),
+                mainPanel(
+                  plotlyOutput("volumePlot"),
+                  br(),
+                  DTOutput("volumeTable")
+                )
+              )
       ),
+      
+      # Onglet Valeur marchande
       tabItem(tabName = "valeur",
               h2("Valeur marchande des transferts par pays et ligue de départ"),
               sidebarLayout(
@@ -41,6 +57,7 @@ ui <- dashboardPage(
                 )
               )
       )
+      
     )
   )
 )
